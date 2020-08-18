5f521bc846d586e679c1d1217700dcece217af2f
Hapo-congbv <68211165+Hapo-congbv@users.noreply.github.com>
2020-08-18 16:54:46 +0700

    setup

--- public/js/app.js
+++ public/js/app.js
@@ -37112,15 +37112,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/style.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/style.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ***!
+  \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/chungtran/Work/laravel-7-scaffold/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/chungtran/Work/laravel-7-scaffold/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\TaiLieu\CongtyHaposoft\project\HapoLearn-v2\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! E:\TaiLieu\CongtyHaposoft\project\HapoLearn-v2\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! E:\TaiLieu\CongtyHaposoft\project\HapoLearn-v2\resources\sass\style.scss */"./resources/sass/style.scss");
 
 
 /***/ })
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/script.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/style.scss', 'public/css')
+    .copy('node_modules/jquery-confirm/css/jquery-confirm.css', 'public/css')
+    .copy('node_modules/jquery-confirm/js/jquery-confirm.js', 'public/js')
+    .copy('node_modules/slick-carousel/slick/slick.min.js', 'public/js')
+    .copy('node_modules/slick-carousel/slick/slick.css', 'public/css')
+    .copy('node_modules/slick-carousel/slick/slick-theme.css', 'public/css');
