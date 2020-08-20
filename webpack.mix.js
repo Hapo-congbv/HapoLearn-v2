f878e17e165835da38b80290f63b49c2bc5cab05
Hapo-congbv <68211165+Hapo-congbv@users.noreply.github.com>
2020-08-20 13:52:18 +0700

    udpate

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,4 +19,5 @@ mix.js('resources/js/app.js', 'public/js')
     .copy('node_modules/jquery-confirm/js/jquery-confirm.js', 'public/js')
     .copy('node_modules/slick-carousel/slick/slick.min.js', 'public/js')
     .copy('node_modules/slick-carousel/slick/slick.css', 'public/css')
-    .copy('node_modules/slick-carousel/slick/slick-theme.css', 'public/css');
+    .copy('node_modules/slick-carousel/slick/slick-theme.css', 'public/css')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
