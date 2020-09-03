91dc5a33cda9791d0bb8523da859976e83fd5927
Hapo-congbv <68211165+Hapo-congbv@users.noreply.github.com>
2020-09-03 09:21:33 +0700

    update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -107,6 +107,5 @@
     <aside class="control-sidebar control-sidebar-dark">
     </aside>
 </div>
-{{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> --}}
 </body>
 </html>
