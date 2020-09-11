d249370b4b474aa7988e9c086e971cb2a1a69572
Hapo-congbv <68211165+Hapo-congbv@users.noreply.github.com>
2020-09-11 16:44:59 +0700

    fix style.scss and user const

--- app/Http/Controllers/User/CourseController.php
+++ app/Http/Controllers/User/CourseController.php
@@ -23,7 +23,7 @@ class CourseController extends Controller
     public function index()
     {
         $courses = Course::paginate(config('variable.pagination'));
-        $teachers = User::where('role_id', '2')->get();
+        $teachers = User::where('role_id', User::ROLE['teacher'])->get();
         $tags = Tag::all();
         return view('course', compact('courses', 'teachers', 'tags'));
     }
